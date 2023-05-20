54ba7c3f0dfbdcca2eec755b4aaba1529ed48b31
munyite001 <emunyite@gmail.com>
2023-05-20 15:02:50 +0300

    First Page

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Nairobi, Kenya',
+        'salary': 'Ksh 100,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Mombas, Kenya',
+        'salary': 'Ksh 120,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': 'Ksh 120,000'
+    },
+    {
+        'id': 1,
+        'title': 'Backend Engineer',
+        'location': 'Nairobi, Kenya',
+        'salary': 'Ksh 200,000'
+    }
+]
 
 @app.route('/')
 def hello():
-    return("Hello World")
+    return render_template("index.html", jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == ("__main__"):
     app.run('0.0.0.0', 5500, debug=True)
