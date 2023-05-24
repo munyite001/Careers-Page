86aba698cc77d8101ef2824387e80f342c392082
munyite001 <emunyite@gmail.com>
2023-05-24 08:43:50 +0300

    dynamic job pages

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, jsonify
-from sqlalchemy import text
-from database import engine
+from database import load_job_from_db, load_jobs
 
 app = Flask(__name__)
-
-#   Function to load jobs from the database
-def load_jobs():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
-
-        #   Convert Sql Alchemy Legacy row to list of dictionaries
-        result_dict = []
-
-        for res in result.all():
-            result_dict.append(dict(res))
-    return result_dict
 
 
 @app.route('/')
 def hello_devtime():
     JOBS = load_jobs()
     return render_template("index.html", jobs=JOBS)
+
+
+@app.route('/job/<id>')
+def show_job(id):
+    job=load_job_from_db(id)
+    if not job:
+        return "Not Found!", 404
+    return render_template("job.html", job=job)
 
 @app.route('/api/jobs')
 def list_jobs():
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 #   Establish connection to database
 try:
@@ -7,3 +7,27 @@ try:
     print("Connection succeeded successfully")
 except Exception as ex:
     print(f"Connection couldn't be made due to the following error: {ex}")
+
+
+def load_jobs():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+
+        #   Convert Sql Alchemy Legacy row to list of dictionaries
+        result_dict = []
+
+        for res in result.all():
+            result_dict.append(dict(res))
+    return result_dict
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val").bindparams(val=id))
+        
+        rows = result.all()
+
+        if len(rows) == 0:
+            return None
+        else:
+            return (dict(rows[0]))
+
